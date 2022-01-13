07554e3992c6225f3140f9772ca8bf08adfed28d
Curtion <curtion@126.com>
2022-01-13 23:48:50 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#![windows_subsystem = "windows"]
 use rodio::buffer::SamplesBuffer;
 use rodio::{source::Source, OutputStream};
 
 fn main() {
-    let st = SamplesBuffer::new(1, 44100, vec![1i16, 2, 3, 4, 5, 6]);
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    stream_handle.play_raw(st.convert_samples()).unwrap();
-    std::thread::sleep(std::time::Duration::from_secs(5));
+    loop {
+        let st = SamplesBuffer::new(1, 44100, vec![1i16, 1, 1, 1, 1, 1]);
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        stream_handle.play_raw(st.convert_samples()).unwrap();
+        std::thread::sleep(std::time::Duration::from_secs(60 * 5));
+    }
 }
