163a731d4a28aff4080bd6f8065cb4b0a413d3ee
Curtion <curtion@126.com>
2022-01-14 21:44:36 +0800

    add wav file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![windows_subsystem = "windows"]
-use rodio::buffer::SamplesBuffer;
-use rodio::{source::Source, OutputStream};
+use rodio::{source::Source, Decoder, OutputStream};
+use std::io::Cursor;
+use std::time::Duration;
 
 fn main() {
     loop {
-        let st = SamplesBuffer::new(1, 44100, vec![1i16, 1, 1, 1, 1, 1]);
         let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        stream_handle.play_raw(st.convert_samples()).unwrap();
-        std::thread::sleep(std::time::Duration::from_secs(60 * 5));
+        let buf = Cursor::new(include_bytes!("../slient.wav"));
+        let source = Decoder::new(buf).expect("音频解码错误");
+        stream_handle
+            .play_raw(source.convert_samples())
+            .expect("播放失败");
+        std::thread::sleep(Duration::from_secs(60 * 5));
     }
 }
